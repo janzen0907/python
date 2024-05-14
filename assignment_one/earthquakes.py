38310d96f37ddfd54beb729138832cfe667522e6
John Janzen <janzen0907@saskpolytech.ca>
2024-05-14 11:46:38 -0600

    Code completed for first section of the assignment

--- assignment_one/earthquakes.py
+++ assignment_one/earthquakes.py
@@ -70,11 +70,108 @@ class QuakeData:
         if data:
             self.quake_array = np.array(data, dtype=dtype)
 
-
-    def set_location_filter(latitude, longitude, distance):
+    def set_location_filter(self, latitude, longitude, distance):
         '''Returns Quakes within the distance the distance of the latitude and longitude being passed in'''
-        # Test commit
-        junk = 'delete me'
+        self.filter_latitude = latitude
+        self.filter_longiture = longitude
+        self.filter_distance = distance
+
+        
+    
+    def set_property_filter(self, magnitude = None, felt = None, significance = None):
+        '''Return only quakes whose magnitude, felt, or significance properties are at least the values supplied'''
+        # Check that one property was passed in
+        if magnitude is None and felt is None and significance is None:
+            raise ValueError("Please supply at least one parameter")
+        
+        self.filter_magnitude = magnitude
+        self.filter_felt = felt
+        self.filter_significance = significance
+    
+    def clear_filter(self):
+        '''Will clear all the filters'''
+        self.filter_distance = None
+        self.filter_felt = None
+        self.filter_latitude = None
+        self.filter_longiture = None
+        self.filter_magnitude = None
+        self.filter_significance = None
+
+    def get_filtered_array(self): 
+        '''Returns a numpy array of only quakes that meet the passed in critera to the filters'''
+        # Check if there is quake data in th quake array
+        if self.quake_array is None:
+            return None
+        
+        # Filters for the location
+        if self.filter_latitude is not None and self.filter_latitude is not None and self.filter_distance is not None:
+            quakes_filtered = []
+
+            for quake in self.quake_array:
+                quake_lat = quake['lat']
+                quake_long = quake['long']
+
+                distance = calc_distance(self.filter_latitude, self.filter_longiture, quake_lat, quake_long)
+
+                if distance <= self.filter_distance:
+                    quakes_filtered.append(quake)
+            return np.array(quakes_filtered)
+        
+        # Filters for the properties. 
+        if self.filter_magnitude is not None or self.filter_felt is not None or self.filter_significance is not None:
+            quakes_filtered = []
+            for quake in self.quake_array:
+                # Using / to break this wonderful if statement onto multipl lines for readability
+                if self.filter_magnitude is None or quake['magnitude'] >= self.filter_magnitude and \
+                self.filter_felt is None or quake['felt'] >= self.filter_felt and \
+                self.filter_significance is None or quake['significance'] >= self.filter_significance:
+                    return np.array(quakes_filtered)
+        
+        # No location was set, return the original array
+        return self.quake_array
+    
+    def get_filtered_list(self):
+        '''Return a list of Quake objects containing the quakes that met the above filters'''
+        # Call the function to get a filtered array
+        filtered_array = self.get_filtered_array()
+        
+        # Ensure it contains data
+        if filtered_array is not None:
+            filtered_list = []
+            for quake in filtered_array:
+                # Loop through the array and add the quaakes to the list
+                filtered_list.append(quake['quake'])
+            return filtered_list
+        return None
+    
+
+class Quake: 
+    '''Contains data about a quake'''
+    
+    def __init__(self, magnitude, time, felt, significance, q_type, coords):
+        self.magnitude = magnitude
+        self.time = time
+        self.felt = felt
+        self.significance = significance
+        self.q_type = q_type
+        self.lat = coords[0]
+        self.long = coords[1]
+
+    def __str__(self):
+        '''Returns a constructed string to represent a Quake Object'''
+        return f"{self.magnitude} Magnitude Earthquake, {self.significance} Significance, felt by {self.felt} people in ({self.lat}, {self.long})"
+    
+    def get_distance_from(self, latitude, longitude, distance):
+        '''Return the number of KM the quake is away from the passed in point'''
+        # Calling my previously made function for this, I don't think anything really changes so I will call the method
+        calc_distance(self.lat, self.long, latitude, longitude)
+        
+        
+
+
+
+        
+        
 
 
 
